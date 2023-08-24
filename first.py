61e32806fd9499c71051540703a8dabe78a8faee
98Nishi <nishamaurya7499@gmail.com>
2023-08-24 18:05:56 +0530

    started

---
+++ first.py
@@ -0,0 +1,11 @@
+# Receiving value
+
+name = input("What is your name? ")
+print("Hello  " + name)
+
+
+# finding age
+
+birth_year = input("Enter your birth year ")
+age = 2023 - int(birth_year)
+print(age)
