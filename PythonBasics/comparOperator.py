9cf0cf6c2d0d0879689f3feb7b6634d02dd23980
98Nishi <nishamaurya7499@gmail.com>
2023-09-09 19:50:08 +0530

    Operators example

---
+++ PythonBasics/comparOperator.py
@@ -0,0 +1,26 @@
+
+# Equal to ('=='):Checks if two values are equal.
+
+x = 5
+y = 5
+result = x == y  #Output: True
+
+#Not equal to ('!='):Checks if two values are not equal.
+
+x = 5
+y = 3
+result = x != y  #Output: True
+
+#Greater than (>) and Greater than or equal to (>=): Checks if one value is greater than another, or greater than or equal to.
+
+x = 8
+y = 6
+result1 = x > y   #Output: True
+result2 = x >= y  #Output: True
+
+#Less than (<) and Less than or equal to (<=): Checks if one value is less than another, or less than or equal to.
+
+x = 3
+y = 5
+result1 = x < y   #Output: True
+result2 = x <= y  #Output: True
