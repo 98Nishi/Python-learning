4453d25be2f24226320cd472066cd8a9e700442c
98Nishi <nishamaurya7499@gmail.com>
2023-09-11 18:23:52 +0530

    sum problem

---
+++ PythonBasics/cal.py
@@ -0,0 +1,12 @@
+# Sum of two numbers
+
+
+# Input: Two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+result = num1 + num2
+
+# Print the result
+print("The sum of", num1, "and", num2, "is", result)
